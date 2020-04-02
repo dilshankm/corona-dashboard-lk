ce0b1072924813a09de73e3165f44e75136fada4
dilshankm <dilshankm@outlook.com>
2020-04-02 17:47:19 +0530

    Header Resposivness was added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/layout/Header.js'
 import DailyData from './components/DailyData.js'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class App extends React.Component {
   constructor(props) {
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,40 +1,38 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 export default class Header extends Component {
 
     headerStyle = () => {
         return {
-            fontSize: '2rem',
-            height: '4rem'
+            fontSize: '3.5vmin'
         }
     }
 
     navBarStyle = () => {
         return {
             backgroundColor: 'navy',
-            fontSize: '2rem',
-            fontWeight: 'bold'
+            fontWeight: 'bold',
+            width: 'auto',
+            sticky: 'top'
+        }
+    }
+
+    conStyle = () => {
+        return {
+            width: 'auto'
         }
     }
 
 
     render() {
         return (
-            <div>
-                <Navbar style={this.navBarStyle()} variant="dark">
-                    <Navbar.Brand style={this.headerStyle()}>
-                        <img
-                            alt=""
-                            src="https://covid-dashboard-sl.s3.ap-south-1.amazonaws.com/logo.svg"
-                            width="50"
-                            height="50"
-                            className="d-inline-block align-center"
-                        />{' '}
-      COVID 2019 Sri Lanka Dashbaord
-    </Navbar.Brand>
-                </Navbar>
-            </div>
+            <Navbar collapseOnSelect expand="lg" style={this.navBarStyle()} variant="dark">
+                <Navbar.Brand style={this.headerStyle()}>COVID 2019 Sri Lanka Dashbaord </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
+            </Navbar>
         )
     }
 }
